7b1443c6a6e811233aac266a42b5b72b206da8da
Jeff Leung <jleung@v10networks.ca>
2018-04-02 14:45:32 -0700

    Rename namespace to make it look a bit prettier

--- NoFraud.UnitTest/TestMethod.cs
+++ NoFraud.UnitTest/TestMethod.cs
@@ -1,7 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Net;
 using System.Threading.Tasks;
-using V10Networks.NoFraud;
+using V10Networks.FraudDetection;
 
 namespace NoFraud.UnitTest
 {
@@ -11,7 +11,7 @@ namespace NoFraud.UnitTest
         [TestMethod]
         public async Task TestNoFraud()
         {
-            double score = await V10Networks.NoFraud.NoFraud.GetFraudScore(IPAddress.Parse("8.8.8.8"));
+            double score = await V10Networks.FraudDetection.NoFraud.GetFraudScore(IPAddress.Parse("8.8.8.8"));
 
             Assert.AreEqual(0, score, 1, "Fraud check doesn't work!");
         }
